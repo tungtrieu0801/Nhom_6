709cd349b118f46984b06cd54d0a917766455815
Nguyen Huu Toan <nguyenhuutoanhh@gmail.com>
2023-09-19 15:18:53 +0700

    toan sua

--- tu.py
+++ tu.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 n = int(input("Nhap n: "))
-m = int(input("Nhap m: "))
-a = np.zeros((n, m))
+a = np.zeros((n, n))
 b = np.zeros((n))
 
 for i in range(n):
-    for j in range(m):
+    for j in range(n):
         a[i, j] = float(input(f"a{i + 1},{j + 1}: "))
 for i in range(n):
     b[i] = float(input(f"b{i + 1}: "))
-x = np.linalg.solve(a, b)
+det=np.linalg.det(a)
+if det==0:
+    print("he vo nghiem hoac vo so nghiem")
 
+x = np.linalg.solve(a, b)
 print(x)
-tungcmmoott
+
