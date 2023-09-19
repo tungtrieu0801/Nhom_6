d0e812a2ff710e4a44bfc8ab27e07ebddd2af699
Tnus2k2 <120782796+Tnus2k2@users.noreply.github.com>
2023-09-19 13:50:44 +0700

    Add files via upload

---
+++ tu.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+n = int(input("Nhap n: "))
+m = int(input("Nhap m: "))
+a = np.zeros((n, m))
+b = np.zeros((n))
+
+for i in range(n):
+    for j in range(m):
+        a[i, j] = float(input(f"a{i + 1},{j + 1}: "))
+for i in range(n):
+    b[i] = float(input(f"b{i + 1}: "))
+x = np.linalg.solve(a, b)
+
+print(x)
