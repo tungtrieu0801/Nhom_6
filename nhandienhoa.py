72be5826b90e4a53089eb12d11a6c7d39c25d723
Nguyen Huu Toan <nguyenhuutoanhh@gmail.com>
2023-10-08 19:44:40 +0700

    toan commit

---
+++ nhandienhoa.py
@@ -0,0 +1,102 @@
+import numpy as np
+import os
+import PIL
+import matplotlib.pyplot as plt
+import PIL.Image
+import tensorflow as tf
+import pathlib
+
+# tải dữ liệu hình ảnh hoa
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+archive = tf.keras.utils.get_file(origin=dataset_url, extract=True)
+data_dir = pathlib.Path(archive).with_suffix('')
+
+#kiểm tra bao nhiêu dữ liệu hoa
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+#tạo tập dữ liệu
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+#cấu hình tệp dữ liệu cho hiệu suất
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+#dùng tf.data để kiểm soát dữ liệu
+list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'), shuffle=False)
+list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
+
+for f in list_ds.take(5):
+  print(f.numpy())
+
+class_names = np.array(sorted([item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"]))
+print(class_names)
+
+val_size = int(image_count * 0.2)
+train_ds = list_ds.skip(val_size)
+val_ds = list_ds.take(val_size)
+print(tf.data.experimental.cardinality(train_ds).numpy())
+print(tf.data.experimental.cardinality(val_ds).numpy())
+
+def get_label(file_path):
+  # Convert the path to a list of path components
+  parts = tf.strings.split(file_path, os.path.sep)
+  # The second to last is the class-directory
+  one_hot = parts[-2] == class_names
+  # Integer encode the label
+  return tf.argmax(one_hot)
+def decode_img(img):
+  # Convert the compressed string to a 3D uint8 tensor
+  img = tf.io.decode_jpeg(img, channels=3)
+  # Resize the image to the desired size
+  return tf.image.resize(img, [img_height, img_width])
+def process_path(file_path):
+  label = get_label(file_path)
+  # Load the raw data from the file as a string
+  img = tf.io.read_file(file_path)
+  img = decode_img(img)
+  return img, label
+# Set `num_parallel_calls` so multiple images are loaded/processed in parallel.
+train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+val_ds = val_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+
+def configure_for_performance(ds):
+  ds = ds.cache()
+  ds = ds.shuffle(buffer_size=1000)
+  ds = ds.batch(batch_size)
+  ds = ds.prefetch(buffer_size=AUTOTUNE)
+  return ds
+
+train_ds = configure_for_performance(train_ds)
+val_ds = configure_for_performance(val_ds)
+
+image_batch, label_batch = next(iter(train_ds))
+
+plt.figure(figsize=(10, 10))
+for i in range(9):
+  ax = plt.subplot(3, 3, i + 1)
+  plt.imshow(image_batch[i].numpy().astype("uint8"))
+  label = label_batch[i]
+  plt.title(class_names[label])
+  plt.axis("off")
+
+plt.tight_layout()
+plt.show()
