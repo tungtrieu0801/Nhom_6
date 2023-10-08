d9eb14c6c1b96e8bfc241c6bb5e164d11f9784ce
Nguyen Huu Toan <nguyenhuutoanhh@gmail.com>
2023-10-08 19:57:25 +0700

    toan commit

--- nhandienhoa.py
+++ nhandienhoa.py
@@ -18,28 +18,8 @@ print(image_count)
 batch_size = 32
 img_height = 180
 img_width = 180
-
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-
-val_ds = tf.keras.utils.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-
 #cấu hình tệp dữ liệu cho hiệu suất
 AUTOTUNE = tf.data.AUTOTUNE
-train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
-
 #dùng tf.data để kiểm soát dữ liệu
 list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'), shuffle=False)
 list_ds = list_ds.shuffle(image_count, reshuffle_each_iteration=False)
